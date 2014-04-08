73ee6dc8db6327118a78f4c23967786565ebbb12
aljaz.romih <aljaz.romih@gmail.com>
2014-04-08 11:46:58 +0200

    Prvic delam git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projektni::Application.config.secret_token = '8d193fbf658a1915d500c8544f0c113e721ca726d8963c8d884b6c9d2c5d1649716c2a267e5c6b7855540a20850e0e565e58cf611825d9f8127fc571bf98bd5b'
